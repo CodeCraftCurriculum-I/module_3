0e6b339fddcd3a2fffa72c7598fb93fb73794f1e
Christian Robere Simonsen <crsimo99@uiaa220870ab.uia.no>
2022-09-15 15:34:02 +0200

    updated code from lecture

--- code/hangman.mjs
+++ code/hangman.mjs
@@ -1,16 +1,27 @@
+import * as fs from "node:fs"
 import * as readlinePromises from 'node:readline/promises';
 import { HANGMAN_UI } from "./graphics.mjs";
 import {ANSI} from "./ansi.mjs";
 
+
 const rl = readlinePromises.createInterface({input: process.stdin,output: process.stdout});
-const WORD_LIST = ["car", "cat", "dog", "elephant", "fish", "giraffe", "horse", "monkey", "panda", "sheep", "tiger", "zebra", "ant","bee","marmelade","computer"];
+
+const WORD_LIST = ["car", "cat", "dog", "elephant", "fish", "giraffe", "horse", "monkey", "panda", "sheep", "tiger", "zebra", "ant","bee","marmelade","computer","dax","sjøbanan"];
+
+const logging = fs.createWriteStream("./hangman.log",{flags:"a"});
+
+function logg(message){
+    logging.write(message);
+}
 
 let atempts = 0;
 const MAX_ATEMPTS = HANGMAN_UI.length-1;
-const word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+const word = pickRandomWordFromList(WORD_LIST);
 const wordLength = word.length;
 const wordArray = word.split("");
-const wordDisplay = new Array(wordLength).fill("_");
+const wordDisplay = createWordDisplay();
+
+logg(word);
 
 const guesses = [];
 let isPlaing = true;
@@ -19,6 +30,7 @@ write(ANSI.CLEAR_SCREEN)
 
 while(isPlaing){
     
+    write(ANSI.BELL);
     write(ANSI.CLEAR_SCREEN)
     write(ANSI.CURSOR_HOME)
     write("Lets play Hangman!\n");
@@ -30,11 +42,14 @@ while(isPlaing){
     const guess = await rl.question("Your guess: ");
     guesses.push(guess);
     if(wordArray.includes(guess)){
-        wordArray.forEach((letter, index) => {
+        
+        for(let index = 0; index < wordArray.length; index++){
+            const letter = wordArray[index];
             if(letter === guess){
                 wordDisplay[index] = `${ANSI.COLOR.GREEN}${letter}${ANSI.RESET}`;
             }
-        });
+        }
+       
     } else{
         atempts++;
     }
@@ -55,5 +70,14 @@ setTimeout(() => {
 
 
 function write(text) {
+    // Noe alla console.log men uten "\n" på slutten av vær print.
     process.stdout.write(text);
+}
+
+function pickRandomWordFromList(list){
+    return list[Math.floor(Math.random() * WORD_LIST.length)];
+}
+
+function createWordDisplay(wordLenght){
+    return new Array(wordLength).fill("_")
 }
